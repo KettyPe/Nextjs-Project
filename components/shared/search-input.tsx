31a6aecf859af4e95299887ff61aad364992afb8
Yuliya <narutoworkzumaki@gmail.com>
2025-06-25 22:45:57 +0700

    feat: axios, Implementing product search

--- components/shared/search-input.tsx
+++ components/shared/search-input.tsx
@@ -2,21 +2,31 @@
 
 import { cn } from "@/lib/utils"
 import { Search } from "lucide-react";
-import { useClickAway } from 'react-use'
+import { useClickAway, useDebounce } from 'react-use'
 import React from "react";
 import Link from "next/link";
+import { Api } from "@/services/api-client";
+import { Product } from "@prisma/client";
 
 interface Props {
      className?: string
 }
 
 export const SearchInput: React.FC<Props> = ({ className }) => {
+     const [searchQuery, setSearchQuery] = React.useState('')
      const [focused, setFocused] = React.useState(false)
+     const [products, setProducts] = React.useState<Product[]>([])
      const ref = React.useRef(null)
 
      useClickAway(ref, () => {
           setFocused(false)
      })
+
+     useDebounce(() => {
+          Api.products.search(searchQuery).then(items => {
+               setProducts(items)
+          })
+     }, 100, [searchQuery])
 
      return (
           <div>
@@ -28,18 +38,32 @@ export const SearchInput: React.FC<Props> = ({ className }) => {
                          type="text"
                          placeholder="Найти..."
                          onFocus={() => setFocused(true)}
+                         value={searchQuery}
+                         onChange={(e) => setSearchQuery(e.target.value)}
                     />
 
-                    <div className={cn(
-                         'absolute w-full bg-white rounded-xl py-2 top-14 shadow-md transition-all duration-200 invisible opacity-0 z-30',
-                         focused && 'visible opacity-100 top-12',
-                         className
-                    )}>
-                         <Link className="flex items-center gap-3 w-full px-3 py-2 hover:bg-primary/10 pl-10" href="/product/1">
-                              <img className="rounded-sm h-8 w-8" src="https://media.dodostatic.net/image/r:292x292/11ee7d61706d472f9a5d71eb94149304.jpg" alt="Пицца 1"  />
-                              <span>Пицца Пепперони фреш </span>
-                         </Link>
-                    </div>
+                    {products.length > 0 && (
+                         <div className={cn(
+                              'absolute w-full bg-white rounded-xl py-2 top-14 shadow-md transition-all duration-200 invisible opacity-0 z-30',
+                              focused && 'visible opacity-100 top-12',
+                              className
+                         )}>
+                              {products.map((product) => (
+                                   <Link
+                                        key={product.id}
+                                        href={`/product/${product.id}`}
+                                        className="flex items-center gap-3 w-full px-3 py-2 hover:bg-primary/10 pl-10"
+                                   >
+                                        <img
+                                             className="rounded-sm h-8 w-8"
+                                             src={product.imageUrl}
+                                             alt={product.name}
+                                        />
+                                        <span>{product.name}</span>
+                                   </Link>
+                              ))}
+                         </div>
+                    )}
                </div>
           </div>
      )
