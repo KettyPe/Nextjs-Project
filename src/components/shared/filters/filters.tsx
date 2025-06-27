3528b102caf1e691ffed03fbacc441c4238441e6
Yuliya <narutoworkzumaki@gmail.com>
2025-06-27 18:15:10 +0700

    range slider filters

--- src/components/shared/filters/filters.tsx
+++ src/components/shared/filters/filters.tsx
@@ -16,11 +16,26 @@ interface Props {
   className?: string;
 }
 
+interface RangePriceProps {
+  priceFrom: number
+  priceTo: number
+}
+
 export const Filters: React.FC<Props> = ({ className }) => {
   const { ingredients, loading, selectedIds, onAddId } = useFilterIngredients();
+  const [prices, setPrice] = React.useState<RangePriceProps>({
+    priceFrom: 0,
+    priceTo: 1000
+  })
 
-  const itemsIngredients = ingredients.map((item) => ({ value: String(item.id), text: item.name }
-  ))
+  const itemsIngredients = ingredients.map((item) => ({ value: String(item.id), text: item.name }))
+
+  const updatePrice = (name: keyof RangePriceProps, value: number) => {
+    setPrice({
+      ...prices,
+      [name]: value
+    })
+  }
 
   return (
     <div className={cn("", className)}>
@@ -39,11 +54,25 @@ export const Filters: React.FC<Props> = ({ className }) => {
             placeholder="0"
             min={0}
             max={1000}
-            defaultValue={0}
+            value={String(prices.priceFrom)}
+            onChange={(e) => updatePrice('priceFrom', Number(e.target.value))}
           />
-          <Input type="number" placeholder="1000" min={100} max={1000} />
+          <Input
+            type="number"
+            placeholder="1000"
+            min={100}
+            max={1000}
+            value={String(prices.priceTo)}
+            onChange={(e) => updatePrice('priceTo', Number(e.target.value))}
+          />
         </div>
-        <RangeSlider min={0} max={500} step={10} value={[0, 500]} />
+        <RangeSlider
+          min={0}
+          max={500}
+          step={10}
+          value={[prices.priceFrom, prices.priceTo]}
+          onValueChange={([priceFrom, priceTo]) => setPrice({priceFrom, priceTo})}
+        />
       </div>
 
       <div className="mt-5">
